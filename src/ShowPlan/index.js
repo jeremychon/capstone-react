4e40ddfb352840e9a58b99ed9b000fc6fd10fc19
Jeremy Chon <jaedee95@gmail.com>
2019-09-04 23:29:59 -0500

    can update workout plan

--- src/ShowPlan/index.js
+++ src/ShowPlan/index.js
@@ -5,7 +5,12 @@ class ShowPlan extends React.Component {
 		super()
 
 		this.state = {
-			plan: {}
+			editing: false,
+			plan: {},
+			goalType: '',
+			current: '',
+			goal:'',
+			public: false
 		}
 	}
 
@@ -29,7 +34,56 @@ class ShowPlan extends React.Component {
 		const foundPlanResponse = await foundPlan.json()
 
 		this.setState({
-			plan: foundPlanResponse.data
+			plan: foundPlanResponse.data,
+			goalType: foundPlanResponse.data.goalType
+		})
+	}
+
+	editingToggle = (e) => {
+		e.preventDefault()
+
+		this.setState({editing: true})
+	}
+
+	handleChange = (e) => {
+		this.setState({
+			[e.currentTarget.name]: e.currentTarget.value
+		})
+	}
+
+	handleInputChange = (e) => {
+		this.setState({
+			public: e.currentTarget.checked ? true : false
+		})
+	}
+
+	handleSubmit = async (e) => {
+		e.preventDefault()
+
+		const updatedPlan = await fetch('http://localhost:9000/plan/' + this.state.plan._id, {
+			method: 'PUT',
+			credentials: 'include',
+			body: JSON.stringify({
+				goalType: this.state.goalType,
+				current: parseInt(this.state.current),
+				goal: parseInt(this.state.goal),
+				public: this.state.public
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+
+		if (updatedPlan.status !== 200) {
+			throw Error('updatedPlan is not running')
+		}
+
+		const updatedPlanResponse = await updatedPlan.json()
+		console.log(updatedPlanResponse, '<---- updatedPlanResponse');
+
+		this.setState({
+			editing: false,
+			plan: updatedPlanResponse.data
 		})
 	}
 
@@ -38,13 +92,47 @@ class ShowPlan extends React.Component {
 		return (
 			<div>
 				<h1>Show Plan</h1>
-				<div>
-					<div>{this.state.plan.goalType}</div>
-					<div>{this.state.plan.current}</div>
-					<div>{this.state.plan.goal}</div>
-					{this.state.plan.user === this.props.userId ? <button>Edit</button> : null}
-					{this.state.plan.user === this.props.userId ? <button>Delete</button> : null}
-				</div>
+					{this.state.editing ? null : 
+						<div>
+						<div>{this.state.plan.goalType}</div>
+						<div>{this.state.plan.current}</div>
+						<div>{this.state.plan.goal}</div>
+						{this.state.plan.user === this.props.userId ? <button onClick={this.editingToggle}>Edit</button> : null}
+						{this.state.plan.user === this.props.userId ? <button>Delete</button> : null}
+						</div>
+					}
+
+					{this.state.editing ? 
+						<form onSubmit={this.handleSubmit}>
+							<select name="goalType" onChange={this.handleChange}>
+								<option defaultValue={this.state.goalType}>{this.state.plan.goalType}</option>
+								<option value={this.state.plan.goalType === 'Weight loss' ? 'Strength' : 'Weight loss'}>{this.state.plan.goalType === 'Weight loss' ? 'Strength' : 'Weight loss'}</option>
+							</select>
+							<input 
+								type="number" 
+								name="current" 
+								value={this.state.current} 
+								placeholder={this.state.plan.current} 
+								onChange={this.handleChange}
+							/>
+							<input 
+								type="number" 
+								name="goal" 
+								value={this.state.goal} 
+								placeholder={this.state.plan.goal} 
+								onChange={this.handleChange}
+							/>
+							Share?
+							<input 
+								type="checkbox"
+								name="public"
+								checked={this.state.public}
+								onChange={this.handleInputChange}
+							/>
+							<button>Update</button>
+						</form> 
+						: null
+					}
 			</div>
 		)
 	}
