5cb3467ea97ff238d1193319a39fac6ddf6a5a7a
Jeremy Chon <jaedee95@gmail.com>
2019-09-10 14:32:19 -0500

    able to get comments from a plan

--- src/Comment/index.js
+++ src/Comment/index.js
@@ -4,6 +4,38 @@ import CreateComment from './CreateComment'
 import { Comment, Header } from 'semantic-ui-react'
 
 class Comments extends React.Component {
+	constructor() {
+		super()
+
+		this.state = {
+			comments: []
+		}
+	}
+
+	componentDidMount() {
+		this.getComments()
+	}
+
+	getComments = async () => {
+		try {
+			const foundComments = await fetch('http://localhost:9000/comment/' + this.props.plan._id, {
+				method: 'GET',
+				credentials: 'include'
+			})
+
+			if (foundComments.status !== 200) {
+				throw Error('foundComments is not running')
+			}
+
+			const foundCommentsRes = await foundComments.json()
+			console.log(foundCommentsRes, '<---- foundCommentsRes');
+
+			this.setState({comments: foundCommentsRes.data})
+
+		} catch (err) {
+			console.log(err);
+		}
+	}
 
 	addComment = async (comment) => {
 		try {
@@ -20,6 +52,10 @@ class Comments extends React.Component {
 				throw Error('addedComment is not running')
 			}
 
+			const addedCommentRes = await addedComment.json()
+
+			this.setState({comment: [...this.state.comments, addedCommentRes.data]})
+
 		} catch (err) {
 			console.log(err);
 		}
@@ -31,7 +67,7 @@ class Comments extends React.Component {
 			<Comment.Group className='commentSection'>
 				<Header as='h3' dividing>Comments</Header>
 				<CreateComment addComment={this.addComment} /> 
-				<CommentList comments={this.props.comments} />
+				<CommentList comments={this.state.comments} />
 			</Comment.Group>
 		)
 	}
